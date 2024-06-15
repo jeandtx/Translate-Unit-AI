dedc076c59ca0613cd47b1b6d26d28b9b5859d5e
nicvlt <nicolas75d@gmail.com>
2024-06-15 18:58:19 +0200

    Refactor utils/__init__.py to import the 'translate' schema and 'PROMPT_TRANSLATE' constant

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,7 @@
 # utils/__init__.py
 
 from .functions import get_genai_model, get_translation
-from .schemas import *
-from .prompts import *
+from .schemas import translate
+from .prompts import PROMPT_TRANSLATE
 
-__all__ = [
-    "get_genai_model",
-    "get_translation",
-]
+__all__ = ["get_genai_model", "get_translation", "translate", "PROMPT_TRANSLATE"]
